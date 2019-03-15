475079ec6d9c6b7bd68f632fc922e6870f60bbe8
Luis Puhl <luispuhl@gmail.com>
2019-03-15 14:16:16 -0300

    meh and some elint format

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+
 import { Login } from './app/login-screen';
 import { EscolhaMetodo } from './app/escolha-metodo-screen';
 import { Cadastro1 } from "./app/cadastro1";
 import { Cadastro2 } from "./app/cadastro2";
 import { Objetivos } from "./app/objetivos";
-import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 /**
  * TODO: Add Navigation (In Progress)
