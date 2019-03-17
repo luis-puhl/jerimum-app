d6cc2ff7c8ceade111a0046547a19eac6ca7c1dd
Luis Puhl <luispuhl@gmail.com>
2019-03-17 17:51:57 -0300

    Renaming

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-import { Login } from './app/login-screen';
-import { EscolhaMetodo } from './app/escolha-metodo-screen';
-import { Cadastro1 } from './app/cadastro1';
-import { Cadastro2 } from './app/cadastro2';
-import { Objetivos } from './app/objetivos';
+import { Login } from './app/Login';
+import { EscolhaMetodo } from './app/EscolhaMetodo';
+import { Cadastro1 } from './app/Cadastro1';
+import { Cadastro2 } from './app/Cadastro2';
+import { Objetivos } from './app/Objetivos';
 
 /**
  * TODO: Add Navigation (In Progress)
