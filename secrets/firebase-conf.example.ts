db424d6f23697f643c9de3425fdc0a92a10490e6
Luis Puhl <luispuhl@gmail.com>
2019-04-17 19:16:07 -0300

    Adding config example

---
+++ secrets/firebase-conf.example.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'apiKey',
+  authDomain: 'authDomain',
+  databaseURL: 'databaseURL',
+  projectId: 'projectId',
+  storageBucket: 'storageBucket',
+  messagingSenderId: 'messagingSenderId',
+};
